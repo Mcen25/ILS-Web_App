6c0a7d204f84ae502ddf2c2d101d8c682ef88577
Matthew En <86386184+Mcen25@users.noreply.github.com>
2023-11-30 00:28:21 -0500

    Connected to Mongodb

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const uri = "mongodb+srv://Mcen25:<ZCsyZipn0J1fpbnW>@constellations.ketcgbo.mongodb.net/?retryWrites=true&w=majority";
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+  
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }  
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
